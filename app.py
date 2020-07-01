905b956a19936ddb2cd11c29dcf5f147f37ce349
Лейфер Д.А <dleyfer@nsiprosto.ru>
2020-07-01 17:36:52 +0400

    Проектное задание главы 2

--- app.py
+++ app.py
@@ -1,30 +1,38 @@
 from flask import Flask, render_template
 from flask_debugtoolbar import DebugToolbarExtension
+from data import tours as data_tours
+from data import departures as data_departures
+from data import tours as data_title
+from data import tours as data_subtitle
+from data import tours as data_description
+
 
 app = Flask(__name__)
 app.debug = True
-app.config['SECRET_KEY'] = 'xv3gavkxc04n3mzx7oksd6q'
+app.config["SECRET_KEY"] = "xv3gavkxc04n3mzx7oksd6q"
 
 
-@app.route('/')
+@app.route("/")
 def render_index():
-    return render_template('index.html', title='Главная страница Stepik Travel')
+    return render_template("index.html", title=data_title, subtitle=data_subtitle, description=data_description)
 
 
-@app.route('/departures/<departure>/')
+@app.route("/departures/<departure>/")
 def render_departures(departure):
-    return render_template('departure.html')
+    return render_template("departure.html", title=data_title, data_tours=data_tours,
+                           departure=data_departures[departure])
 
 
-@app.route('/tours/<id_tour>')
+@app.route("/tours/<int:id_tour>")
 def render_tours(id_tour):
-    return render_template('tour.html')
+    return render_template(
+        "tour.html", data_tours=data_tours.get(id_tour), title=data_title, departures=data_departures)
 
 
 @app.errorhandler(404)
 def render_not_found(error):
-    return render_template('404.html')
+    return render_template("404.html", err=error)
 
 
 toolbar = DebugToolbarExtension(app)
-app.run('127.0.0.1', 8000)
+app.run("127.0.0.1", 8000)
