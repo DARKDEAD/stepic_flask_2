3554edbebc4b94da3f3e201ad2a29da637f3ec89
Лейфер Д.А <dleyfer@nsiprosto.ru>
2020-07-03 14:37:38 +0400

    Проектное задание главы 2

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template
-from flask_debugtoolbar import DebugToolbarExtension
+
+# from flask_debugtoolbar import DebugToolbarExtension
 from data import tours as data_tours
 from data import departures as data_departures
-from data import tours as data_title
-from data import tours as data_subtitle
-from data import tours as data_description
-
+from data import title as data_title
+from data import subtitle as data_subtitle
+from data import description as data_description
 
 app = Flask(__name__)
 app.debug = True
@@ -14,19 +14,60 @@ app.config["SECRET_KEY"] = "xv3gavkxc04n3mzx7oksd6q"
 
 @app.route("/")
 def render_index():
-    return render_template("index.html", title=data_title, subtitle=data_subtitle, description=data_description)
+    log_data_tours = {}
+    amount = 1
+
+    for d_tours_key, d_tour_value in data_tours.items():
+        if amount <= 6:
+            d_tour_value["stars"] = int(d_tour_value["stars"])
+            log_data_tours[d_tours_key] = d_tour_value
+            amount += 1
+        else:
+            break
+
+    return render_template(
+        "index.html",
+        title=data_title,
+        subtitle=data_subtitle,
+        description=data_description,
+        departures=data_departures,
+        tours=log_data_tours,
+    )
 
 
-@app.route("/departures/<departure>/")
-def render_departures(departure):
-    return render_template("departure.html", title=data_title, data_tours=data_tours,
-                           departure=data_departures[departure])
+@app.route("/departures/<id_departure>/")
+def render_departures(id_departure):
+    new_departure = {}
+    cost_tours = []
+    amount_nights = []
+
+    for d_tour_key, d_tour_value in data_tours.items():
+        if d_tour_value.get("departure") == id_departure:
+            d_tour_value["stars"] = int(d_tour_value["stars"])
+            new_departure[d_tour_key] = d_tour_value
+            cost_tours.append(d_tour_value.get("price"))
+            amount_nights.append(d_tour_value.get("nights"))
+
+    return render_template(
+        "departure.html",
+        title=data_title,
+        data_tours=new_departure,
+        departure=data_departures[id_departure],
+        departures=data_departures,
+        cost_tours=cost_tours,
+        amount_nights=amount_nights,
+    )
 
 
 @app.route("/tours/<int:id_tour>")
 def render_tours(id_tour):
     return render_template(
-        "tour.html", data_tours=data_tours.get(id_tour), title=data_title, departures=data_departures)
+        "tour.html",
+        data_tours=data_tours.get(id_tour),
+        title=data_title,
+        departures=data_departures,
+        stars=int(data_tours.get(id_tour)["stars"]),
+    )
 
 
 @app.errorhandler(404)
@@ -34,5 +75,5 @@ def render_not_found(error):
     return render_template("404.html", err=error)
 
 
-toolbar = DebugToolbarExtension(app)
+# toolbar = DebugToolbarExtension(app)
 app.run("127.0.0.1", 8000)
